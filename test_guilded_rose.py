110f53afbe5f37eb7fd322f7cb08028b8573d730
Anitha Gopinath <78701588+AnithaG-Oak@users.noreply.github.com>
2023-03-31 10:56:54 +0530

    Added sample test

--- test_guilded_rose.py
+++ test_guilded_rose.py
@@ -4,10 +4,12 @@ from guilded_rose import Item, update_quality
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_sulfuras_quality(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 2, 80)]
         update_quality(items)
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEquals(80, items[0].quality)
+        self.assertEquals(2, items[0].sell_in)
 
 
 if __name__ == "__main__":
