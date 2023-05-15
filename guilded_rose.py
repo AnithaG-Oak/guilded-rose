f56b3a3f1134ad6b35a76cf4cde4d11350a4db8a
Abhijeet Kumar Sharma <Abhijeet.Sharma@oaknorth.co.uk>
2023-05-15 12:23:58 +0530

    refactored for common item operation

--- guilded_rose.py
+++ guilded_rose.py
@@ -7,34 +7,27 @@ def update_quality(items):
     for item in items:
         if item.name == "Sulfuras, Hand of Ragnaros":
             continue
-        elif item.name == "Aged Brie":
+        item.sell_in = item.sell_in - 1    
+        if item.name == "Aged Brie":
             item.quality = item.quality + 1
-            item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                item.quality = item.quality + 1
-            if item.quality>50:
-                item.quality=50  
-            continue
+                item.quality = item.quality + 1 
         elif item.name == "Backstage passes to a TAFKAL80ETC concert":
             item.quality = item.quality + 1
-            if item.sell_in < 11:
+            if item.sell_in < 10:
                 item.quality = item.quality + 1
-            if item.sell_in < 6:
+            if item.sell_in < 5:
                 item.quality = item.quality + 1  
-            item.sell_in = item.sell_in - 1    
             if item.sell_in < 0:
-                item.quality = 0
-            if item.quality>50:
-                item.quality=50    
-            continue   
+                item.quality = 0 
         else: 
             if item.quality > 0:
                 item.quality = item.quality - 1 
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.quality > 0:
-                    item.quality = item.quality - 1 
-            continue 
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality = item.quality - 1 
+             
+        if item.quality>50:
+            item.quality=50 
 
     return items
 
