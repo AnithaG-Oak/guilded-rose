efdec5c8df971e3a40285644507da43a379cd106
Abhijeet Kumar Sharma <Abhijeet.Sharma@oaknorth.co.uk>
2023-05-15 12:19:20 +0530

    refactored for individual item types

--- guilded_rose.py
+++ guilded_rose.py
@@ -7,29 +7,35 @@ def update_quality(items):
     for item in items:
         if item.name == "Sulfuras, Hand of Ragnaros":
             continue
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                item.quality = item.quality - 1
-        else:   
+        elif item.name == "Aged Brie":
             item.quality = item.quality + 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.sell_in < 11:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
                 item.quality = item.quality + 1
-        if item.quality>50:
-            item.quality=50        
-                    
+            if item.quality>50:
+                item.quality=50  
+            continue
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            item.quality = item.quality + 1
+            if item.sell_in < 11:
+                item.quality = item.quality + 1
+            if item.sell_in < 6:
+                item.quality = item.quality + 1  
+            item.sell_in = item.sell_in - 1    
+            if item.sell_in < 0:
+                item.quality = 0
+            if item.quality>50:
+                item.quality=50    
+            continue   
+        else: 
+            if item.quality > 0:
+                item.quality = item.quality - 1 
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if item.quality > 0:
+                    item.quality = item.quality - 1 
+            continue 
+
     return items
 
 
