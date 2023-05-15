03487ed905c275c73240e2f064259b99b853bdba
Abhijeet Kumar Sharma <Abhijeet.Sharma@oaknorth.co.uk>
2023-05-15 11:54:04 +0530

    refactored for max quantity check

--- guilded_rose.py
+++ guilded_rose.py
@@ -10,16 +10,13 @@ def update_quality(items):
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
             if item.quality > 0:
                 item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+        else:   
+            item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 11:
+                    item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if item.name != "Aged Brie":
@@ -29,8 +26,10 @@ def update_quality(items):
                 else:
                     item.quality = item.quality - item.quality
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                item.quality = item.quality + 1
+        if item.quality>50:
+            item.quality=50        
+                    
     return items
 
 
